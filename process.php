55280eea723d88dce62544eca8b3aa7dcaffc368
omrajak <75496281+omrajak@users.noreply.github.com>
2020-12-04 23:04:54 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,61 @@
+<?php 
+session_start();
+
+
+$mysqli = new mysqli('localhost','root','','crud') or die(mysqli_error($mysqli));
+$update = false; 
+$name='';
+$location='';
+$id=0;
+
+if (isset($_POST['save'])){
+    $name=$_POST['name'];
+    $location=$_POST['location'];
+
+    $mysqli->query("INSERT INTO data(name,location)VALUE('$name','$location')")or
+         die($mysqli->error);
+
+    $_SESSION['message'] ="record has been saved!";
+    $_SESSION['mgs_type'] ="success";
+    
+    header("location: index.php");
+}
+ 
+
+     
+if (isset($_GET['delete'])){
+     $id = $_GET['delete'];
+     $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error);
+
+     $_SESSION['message'] ="record has been deleted!";
+     $_SESSION['mgs_type'] ="danger";
+
+     header("location: index.php");
+ 
+}
+
+if (isset($_GET['edit'])){
+     $id=$_GET['edit'];
+     $update = true;
+     $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error);
+     if (@count($result)==1){
+          $row = $result->fetch_array();
+          $name = $row['name'];
+          $location = $row['location'];
+
+     }
+}
+
+if (isset($_POST['update'])){
+     $id = $_POST['id'];
+     $name = $_POST['name'];
+     $location = $_POST['location'];
+
+    $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id=$id")or
+          die($mysqli->error);
+          
+     $_SESSION['MESSAGE']="record has been updated!";
+     $_SESSION['msg_type'] = "warning";
+     header('location: index.php');
+}
+ ?>
